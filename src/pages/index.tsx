cf2d22bec936f2aa5c776b479761411f12a658a4
Nelson Chinedu <egwuchinedu69@gmail.com>
2021-07-20 15:37:50 +0100

    remove comment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,6 @@
 import dynamic from 'next/dynamic';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
-import { MuiThemeProvider } from '@material-ui/core/styles';
-
-import theme from 'src/components/Theme';
 
 const Landing = dynamic(() => import('src/components/MainLayout/Landing'), {
   loading: () => (
@@ -23,9 +20,7 @@ const Landing = dynamic(() => import('src/components/MainLayout/Landing'), {
 
 const Index = () => {
   return (
-    // <MuiThemeProvider theme={theme}>
-      <Landing />
-    // </MuiThemeProvider>
+    <Landing />
   );
 };
 
